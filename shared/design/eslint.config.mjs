ea9107c1ab6afa84ee87876e1164124d4d1a9ed1
whdgur5717 <whdgur57117@gmail.com>
2025-06-16 20:52:44 +0900

    fix eslint issue - remove eslint-config-next - apply storybook-eslint

--- shared/design/eslint.config.mjs
+++ shared/design/eslint.config.mjs
@@ -1,6 +1,15 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import base from "@ject-5-fe/eslint/base"
 import storybook from "eslint-plugin-storybook"
 
-import base from "@ject-5-fe/eslint/base"
-
-export default base
+export default [
+  ...base,
+  ...storybook.configs["flat/recommended"],
+  {
+    files: ["**/*.stories.tsx"],
+    rules: {
+      "storybook/csf-component": "error",
+      "storybook/default-exports": "off",
+    },
+  },
+]
--- shared/eslint/next.js
+++ shared/eslint/next.js
@@ -1,29 +1,15 @@
 import { FlatCompat } from "@eslint/eslintrc"
 import { defineConfig } from "eslint/config"
 import baseConfig from "./base.js"
-
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-})
+import path from "path"
+import { fileURLToPath } from "url"
+import nextPlugin from "@next/eslint-plugin-next"
 
 export default defineConfig([
   ...baseConfig,
-  ...compat.config({
-    extends: ["next", "next/core-web-vitals", "next/typescript"],
-    rules: {
-      "no-unused-vars": "off",
-      "simple-import-sort/imports": "error",
-      "simple-import-sort/exports": "error",
-      "@typescript-eslint/no-explicit-any": "warn",
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        {
-          ignoreRestSiblings: true,
-          caughtErrors: "none",
-          varsIgnorePattern: "^_",
-          argsIgnorePattern: "^_",
-        },
-      ],
-    },
-  }),
+  nextPlugin.flatConfig.recommended,
+  nextPlugin.flatConfig.coreWebVitals,
+  {
+    ignores: ["node_modules", ".next"],
+  },
 ])
