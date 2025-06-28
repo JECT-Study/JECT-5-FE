4bba74d8aa3553a055857793ce6c9b5e7498a469
whdgur5717 <whdgur57117@gmail.com>
2025-06-28 19:56:37 +0900

    chore: remove example files and code

--- shared/design/src/App.tsx
+++ shared/design/src/App.tsx
@@ -1,14 +1,5 @@
-import { useState } from "react"
-
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div className="flex-1 text-red-100">{count}</div>
-      <button onClick={() => setCount(count + 1)}>+</button>
-    </>
-  )
+  return <div></div>
 }
 
 export default App
