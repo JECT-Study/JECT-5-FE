98b69f997a0b6eaf11f6854c05ea324a8800aefc
whdgur5717 <whdgur57117@gmail.com>
2025-06-30 23:40:34 +0900

    chore: extends commitlint prefix rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "test",
+        "chore",
+        "ci",
+        "build",
+        "revert",
+        "rule", // Cursor Rules 관련 변경사항
+      ],
+    ],
+  },
 }
