cf358070fccd4c46391f1cf221b152f35ae20f75
kimnamheeee <nami200284@snu.ac.kr>
2025-07-10 20:17:52 +0900

    feat(mocks): implement MSW initialization and component for mock service worker

---
+++ service/app/src/mocks/mswComponent.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+
+export const MSWComponent = ({ children }: { children: React.ReactNode }) => {
+  const [mswReady, setMswReady] = useState(false);
+  const hasStartedRef = useRef(false);
+
+  useEffect(() => {
+    const init = async () => {
+      if (hasStartedRef.current) return;
+      hasStartedRef.current = true;
+
+      const { initMsw } = await import("./index");
+      if (process.env.NODE_ENV !== "production") {
+        await initMsw();
+      }
+      setMswReady(true);
+    };
+
+    if (!mswReady) {
+      init();
+    }
+  }, [mswReady]);
+
+  return <>{children}</>;
+};
