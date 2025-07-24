87023347c137e16da0e091c8f825f181995534b5
kimnamheeee <nami200284@snu.ac.kr>
2025-07-24 15:10:48 +0900

    refactor(game): apply utility error handling function to game APIs

--- service/app/src/entities/game/api/getGameDetail.ts
+++ service/app/src/entities/game/api/getGameDetail.ts
@@ -3,14 +3,15 @@ import { UUID } from "@shared/types/common";
 
 import { GameDetailResponse } from "../model";
 import { mapStatusToErrorResponse } from "../utils";
+import { ApiResponse } from "@shared/types/response";
 
-export const getGameDetail = async (gameId: UUID): Promise<GameDetailResponse> => {
+export const getGameDetail = async (gameId: UUID): Promise<GameDetailResponse | ApiResponse<null>>  => {
     const response = await fetchClient.fetch(`/games/${gameId}`, {
       method: 'GET',
     });
 
     if (!response.ok) {
-      mapStatusToErrorResponse(response.status, gameId);
+      return mapStatusToErrorResponse(response.status, gameId);
     }
 
     return response.json();
--- service/app/src/entities/game/api/shareGame.ts
+++ service/app/src/entities/game/api/shareGame.ts
@@ -1,6 +1,7 @@
 import { fetchClient } from "@shared/lib/fetchClient";
 import { UUID } from "@shared/types/common";
 import { ApiResponse } from "@shared/types/response";
+import { mapStatusToErrorResponse } from "../utils";
 
 export const shareGame = async (gameId: UUID): Promise<ApiResponse<null>> => {
   const response = await fetchClient.fetch(`/games/${gameId}/share`, {
@@ -8,7 +9,7 @@ export const shareGame = async (gameId: UUID): Promise<ApiResponse<null>> => {
   });
 
   if (!response.ok) {
-    throw new Error(`Failed to share game: ${response.statusText}`);
+    return mapStatusToErrorResponse(response.status, gameId);
   }
 
   return response.json();
@@ -20,7 +21,7 @@ export const unshareGame = async (gameId: UUID): Promise<ApiResponse<null>> => {
   });
 
   if (!response.ok) {
-    throw new Error(`Failed to unshare game: ${response.statusText}`);
+    return mapStatusToErrorResponse(response.status, gameId);
   }
 
   return response.json();
