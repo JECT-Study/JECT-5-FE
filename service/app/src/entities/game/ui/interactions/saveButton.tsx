d118c3533b6425692cb955ad8319af62fef80e29
kimnamheeee <nami200284@snu.ac.kr>
2025-08-05 18:00:21 +0900

    fix(game): validate questions before saving and show error popup if validation fails

--- service/app/src/entities/game/ui/interactions/saveButton.tsx
+++ service/app/src/entities/game/ui/interactions/saveButton.tsx
@@ -1,11 +1,21 @@
 import { PrimaryBoxButton } from "@shared/design/src/components/button"
 
+import { validateQuestion } from "../../model"
+import { useGameCreationContext } from "../../model/state/create/gameCreationContext"
 import { useGamePopupActions } from "../../model/useGamePopupActions"
 
 export function SaveButton() {
-  const { showSaveConfirm } = useGamePopupActions()
+  const { state } = useGameCreationContext()
+  const { showSaveConfirm, showValidationError } = useGamePopupActions()
 
   const handleSave = () => {
+    const hasValidationError = state.questions.some(question => !validateQuestion(question))
+    
+    if (hasValidationError) {
+      showValidationError()
+      return
+    }
+    
     showSaveConfirm()
   }
 
