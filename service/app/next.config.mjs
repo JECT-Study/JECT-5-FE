fe4c36f770532a9dcadf5dc54b9b2db373202e95
whdgur5717 <whdgur57117@gmail.com>
2025-08-14 14:31:32 +0900

    fix(app/config): fix picsum hostname

--- service/app/next.config.mjs
+++ service/app/next.config.mjs
@@ -25,7 +25,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "*.picsum.photos",
+        hostname: "picsum.photos",
       },
       {
         protocol: "https",
