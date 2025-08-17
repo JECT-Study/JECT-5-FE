9b0c06fb70074408a05afbb82ae45cc9c3402ab0
whdgur5717 <whdgur57117@gmail.com>
2025-08-18 03:14:43 +0900

    chore(playwright): change options to enhance speed

--- service/app/playwright.config.ts
+++ service/app/playwright.config.ts
@@ -4,8 +4,8 @@ export default defineConfig({
   testMatch: "**/*.spec.ts",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  retries: process.env.CI ? 0 : 0,
+  workers: process.env.CI ? 2 : undefined,
   reporter: "html",
   use: {
     baseURL: process.env.PLAYWRIGHT_BASE_URL || "http://localhost:3000",
