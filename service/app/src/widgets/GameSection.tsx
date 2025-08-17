2492f5bf91ae455836aa30af75f4abf1d5681c94
kimnamheeee <nami200284@snu.ac.kr>
2025-08-17 13:36:10 +0900

    refactor(widget): update text color in GameSection and HeroSection components

--- service/app/src/widgets/GameSection.tsx
+++ service/app/src/widgets/GameSection.tsx
@@ -82,7 +82,7 @@ export const GameSection = ({ className = "" }: GameSectionProps) => {
         className={`flex w-full flex-col items-center gap-[45px] ${className}`}
       >
         <div className="flex w-[952px] items-center justify-between">
-          <h2 className="typography-heading-lg-semibold text-neutral-black">
+          <h2 className="typography-heading-lg-semibold text-text-interactive-secondary">
             어떤 게임으로 시작해 볼까요?
           </h2>
           <PrimaryBoxButton
@@ -106,10 +106,10 @@ export const GameSection = ({ className = "" }: GameSectionProps) => {
     <section
       className={`flex w-full flex-col items-center gap-[45px] ${className}`}
     >
-      <div className="flex w-[952px] items-center justify-between">
-        <h2 className="typography-heading-lg-semibold text-neutral-black">
-          어떤 게임으로 시작해 볼까요?
-        </h2>
+              <div className="flex w-[952px] items-center justify-between">
+          <h2 className="typography-heading-lg-semibold text-text-interactive-secondary">
+            어떤 게임으로 시작해 볼까요?
+          </h2>
         <PrimaryBoxButton
           size="md"
           _style="solid"
