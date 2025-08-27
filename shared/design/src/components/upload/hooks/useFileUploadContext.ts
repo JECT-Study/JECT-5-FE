9a91a7774fa73e2d19813076e8707775a7af0158
kimnamheeee <nami200284@snu.ac.kr>
2025-08-27 15:19:06 +0900

    refactor(upload): replace imports to named imports

--- shared/design/src/components/upload/hooks/useFileUploadContext.ts
+++ shared/design/src/components/upload/hooks/useFileUploadContext.ts
@@ -1,4 +1,4 @@
-import * as React from "react"
+import { createContext, useContext } from "react"
 
 import type {
   Direction,
@@ -16,30 +16,29 @@ const ITEM_PROGRESS_NAME = "FileUploadItemProgress"
 const ITEM_DELETE_NAME = "FileUploadItemDelete"
 const CLEAR_NAME = "FileUploadClear"
 
-const DirectionContext = React.createContext<Direction | undefined>(undefined)
+const DirectionContext = createContext<Direction | undefined>(undefined)
 
-export function useDirection(dirProp?: Direction): Direction {
-  const contextDir = React.useContext(DirectionContext)
+function useDirection(dirProp?: Direction): Direction {
+  const contextDir = useContext(DirectionContext)
   return dirProp ?? contextDir ?? "ltr"
 }
 
-const FileUploadContext = React.createContext<FileUploadContextValue | null>(
-  null,
-)
+const FileUploadContext = createContext<FileUploadContextValue | null>(null)
 
-export function useFileUploadContext(consumerName: string) {
-  const context = React.useContext(FileUploadContext)
+function useFileUploadContext(consumerName: string) {
+  const context = useContext(FileUploadContext)
   if (!context) {
     throw new Error(`\`${consumerName}\` must be used within \`${ROOT_NAME}\``)
   }
   return context
 }
 
-const FileUploadItemContext =
-  React.createContext<FileUploadItemContextValue | null>(null)
+const FileUploadItemContext = createContext<FileUploadItemContextValue | null>(
+  null,
+)
 
-export function useFileUploadItemContext(consumerName: string) {
-  const context = React.useContext(FileUploadItemContext)
+function useFileUploadItemContext(consumerName: string) {
+  const context = useContext(FileUploadItemContext)
   if (!context) {
     throw new Error(`\`${consumerName}\` must be used within \`${ITEM_NAME}\``)
   }
@@ -59,4 +58,7 @@ export {
   LIST_NAME,
   ROOT_NAME,
   TRIGGER_NAME,
+  useDirection,
+  useFileUploadContext,
+  useFileUploadItemContext,
 }
