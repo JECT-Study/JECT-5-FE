1f9ce2d64e7851073ad3d368b8aff2287d02364d
whdgur5717 <whdgur5717@gmail.com>
2025-08-29 15:26:15 +0900

    Revert "test: test"
    This reverts commit a6cae5c371699c859f58effbd39c88ae20147237.

--- service/app/src/app/game/[gameId]/play/components/gamePlayContent.tsx
+++ service/app/src/app/game/[gameId]/play/components/gamePlayContent.tsx
@@ -13,12 +13,10 @@ interface GamePlayContentProps {
 
 export const GamePlayContent = ({ currentQuestion }: GamePlayContentProps) => {
   const [showAnswer, setShowAnswer] = useState(false)
+
   return (
     <div className="flex w-full flex-1 flex-col items-center justify-center">
-      <h1
-        className="typography-heading-4xl-bold mb-[118px] max-w-[1080px] text-center text-text-primary"
-        id="special"
-      >
+      <h1 className="typography-heading-4xl-bold mb-[118px] max-w-[1080px] text-center text-text-primary">
         {currentQuestion.questionText}
       </h1>
 
@@ -41,7 +39,6 @@ export const GamePlayContent = ({ currentQuestion }: GamePlayContentProps) => {
         <SecondaryOutlineBoxButton
           size="lg"
           onClick={() => setShowAnswer(false)}
-          aria-label="wefwefwef"
         >
           {currentQuestion?.questionAnswer}
         </SecondaryOutlineBoxButton>
