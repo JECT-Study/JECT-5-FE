d30543285c3eb052dc801d11558e4be2cec0b98d
whdgur5717 <whdgur5717@gmail.com>
2025-09-08 15:06:36 +0900

    feat(token/plugin): apply spacing, radius token in plugin

--- shared/design/src/plugin/index.ts
+++ shared/design/src/plugin/index.ts
@@ -2,10 +2,13 @@ import plugin from "tailwindcss/plugin"
 import type { CSSRuleObject } from "tailwindcss/types/config"
 
 import { generateColorTokens } from "../tokens/color"
+import { generateRadiusTokens } from "../tokens/radius"
+import { generateSpacingTokens } from "../tokens/spacing"
 import {
   fontWeightTokens,
   generateTypographyTokens,
 } from "../tokens/typography"
+import { generateUnitTokens } from "../tokens/unit"
 
 const {
   base: colorBase,
@@ -13,6 +16,9 @@ const {
   theme: colorTheme,
 } = generateColorTokens()
 const { theme: typographyTheme } = generateTypographyTokens()
+const { theme: spacingTheme } = generateSpacingTokens()
+const { theme: radiusTheme } = generateRadiusTokens()
+const { cssVars: unitCssVars } = generateUnitTokens()
 
 export default plugin(
   ({ addBase, addComponents, theme }) => {
@@ -20,6 +26,7 @@ export default plugin(
       ":root": {
         ...fontWeightTokens,
         ...colorBase,
+        ...unitCssVars,
       },
       ".dark": {
         ...colorDark,
@@ -40,7 +47,9 @@ export default plugin(
   },
   {
     theme: {
+      spacing: spacingTheme,
       extend: {
+        borderRadius: radiusTheme,
         colors: colorTheme,
         typography: typographyTheme,
         fontWeight: fontWeightTokens,
