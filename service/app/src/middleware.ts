2130a2563a3ff54c62d07dfcfbb6ae0dac028c2f
kimnamheeee <nami200284@snu.ac.kr>
2025-09-10 14:31:53 +0900

    fix(middleware): adjust logic to accept public asset files

--- service/app/src/middleware.ts
+++ service/app/src/middleware.ts
@@ -8,9 +8,23 @@ export const config = {
 
 export function middleware(request: NextRequest) {
   const { device } = userAgent(request)
-  if (device.type === "mobile") {
-    request.nextUrl.pathname = "/mobile"
-    return NextResponse.rewrite(request.nextUrl)
+  const accept = request.headers.get("accept")
+  const isPageNavigation =
+    request.method === "GET" && accept?.includes("text/html")
+
+  const isPublicAsset =
+    request.nextUrl.pathname.startsWith("/_next") ||
+    request.nextUrl.pathname.startsWith("/api") ||
+    /\.(?:svg|png|jpg|jpeg|gif|webp|ico|css|js|map|txt|xml)$/i.test(
+      request.nextUrl.pathname,
+    )
+
+  if (
+    isPageNavigation &&
+    !isPublicAsset &&
+    (device.type === "mobile" || device.type === "tablet")
+  ) {
+    return NextResponse.rewrite(new URL("/mobile", request.url))
   }
 
   if (
