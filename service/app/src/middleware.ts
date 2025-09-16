67133942a636003e973f88a8839b60720a4f012e
kimnamheeee <nami200284@snu.ac.kr>
2025-09-16 14:21:20 +0900

    fix(middleware): fix function call for runtime compability

--- service/app/src/middleware.ts
+++ service/app/src/middleware.ts
@@ -1,13 +1,16 @@
 import { NextRequest, NextResponse } from "next/server"
 
-import { hasValidSession } from "./entities/auth/utils/cookieUtils"
+const hasValidSessionFromRequest = (req: NextRequest) => {
+  const sid = req.cookies.get("JSESSIONID")?.value
+  return !!sid
+}
 
 export const config = {
   matcher: ["/dashboard/:path*", "/create/:path*"],
 }
 
 export function middleware(request: NextRequest) {
-  if (!hasValidSession()) {
+  if (!hasValidSessionFromRequest(request)) {
     return NextResponse.redirect(new URL("/?message=unauthorized", request.url))
   }
   return NextResponse.next()
