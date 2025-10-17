0995e472209101ef7e30fd5d31cd3d79015821cc
whdgur5717 <whdgur5717@gmail.com>
2025-10-17 20:43:19 +0900

    feat(api/cloneGame): update response type for cloneGame

--- service/app/src/entities/game/api/cloneGame.ts
+++ service/app/src/entities/game/api/cloneGame.ts
@@ -2,6 +2,8 @@ import { fetchClient } from "@/shared/api/fetchClient"
 import type { UUID } from "@/shared/api/types/common"
 
 export const cloneGame = async (gameId: UUID) => {
-  const response = await fetchClient.post<null>(`games/${gameId}/clone`)
+  const response = await fetchClient.post<{ gameId: string }>(
+    `games/${gameId}/clone`,
+  )
   return response.json()
 }
