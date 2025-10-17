2e94a6cc4cd9580921b6295e8f651f132dc0574a
whdgur5717 <whdgur5717@gmail.com>
2025-10-17 20:46:59 +0900

    refactor(games/navigation): change query handling using url with nuqs

--- service/app/src/widgets/GamesNavigation.tsx
+++ service/app/src/widgets/GamesNavigation.tsx
@@ -4,47 +4,50 @@ import * as TextField from "@ject-5-fe/design/components/textField"
 import { Navigation } from "@shared/design/src/components/navigation"
 import { Magnifier } from "@shared/design/src/icons"
 import dynamic from "next/dynamic"
+import { parseAsString, useQueryState } from "nuqs"
 
 import { ThemeToggle } from "@/shared/themeToggleButton"
 
 import { HomeButton } from "./components/homeButton"
 
-const GamesAuthButton = dynamic(() => import("@/shared/authButton"), {
+const LoginButton = dynamic(() => import("@/shared/authButton"), {
   ssr: false,
 })
 
 interface GamesNavigationProps {
   className?: string
-  searchQuery?: string
-  onSearchChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export const GamesNavigation = ({
-  className,
-  searchQuery = "",
-  onSearchChange,
-}: GamesNavigationProps) => {
+export const GamesNavigation = ({ className }: GamesNavigationProps) => {
+  const [query, setQuery] = useQueryState(
+    "query",
+    parseAsString.withDefault(""),
+  )
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const nextValue = event.target.value
+    setQuery(nextValue ? nextValue : null)
+  }
+
   return (
     <Navigation
       className={className}
       leftContent={<HomeButton />}
       centerContent={
-        <div className="hidden w-full max-w-[871px] md:flex">
-          <TextField.Root name="game" className="w-full">
-            <TextField.InputWrapper>
-              <Magnifier className="size-32 text-icon-interactive-input-default" />
-              <TextField.Input
-                value={searchQuery}
-                onChange={onSearchChange}
-                placeholder="오늘의 추천 게임은?"
-              />
-            </TextField.InputWrapper>
-          </TextField.Root>
-        </div>
+        <TextField.Root name="game" className="w-full">
+          <TextField.InputWrapper>
+            <Magnifier className="size-32 text-icon-interactive-input-default" />
+            <TextField.Input
+              value={query}
+              onChange={handleSearchChange}
+              placeholder="오늘의 추천 게임은?"
+            />
+          </TextField.InputWrapper>
+        </TextField.Root>
       }
       rightContent={
         <>
-          <GamesAuthButton />
+          <LoginButton />
           <ThemeToggle />
         </>
       }
