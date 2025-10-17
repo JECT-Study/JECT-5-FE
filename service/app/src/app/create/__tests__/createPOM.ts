a17a3eddd604f11d3a40aa6e9d4e1e6eadcc8800
kimnamheeee <nami200284@snu.ac.kr>
2025-10-17 17:52:21 +0900

    test(create): clear input before fill

--- service/app/src/app/create/__tests__/createPOM.ts
+++ service/app/src/app/create/__tests__/createPOM.ts
@@ -136,10 +136,12 @@ export class CreatePOM {
   }
 
   async fillQuestionInput(question: string) {
+    await this.questionInput.clear()
     await this.questionInput.fill(question)
   }
 
   async fillAnswerInput(answer: string) {
+    await this.answerInput.clear()
     await this.answerInput.fill(answer)
   }
 
@@ -149,6 +151,7 @@ export class CreatePOM {
   }
 
   async fillGameName(gameName: string) {
+    await this.gameNameInput.clear()
     await this.gameNameInput.fill(gameName)
   }
 
