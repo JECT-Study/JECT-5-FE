0c03b89d64a13e03e64c9ee176403921b8aa6892
whdgur5717 <whdgur5717@gmail.com>
2025-10-17 21:49:08 +0900

    feat(games/page): apply Suspense

--- service/app/src/app/games/page.tsx
+++ service/app/src/app/games/page.tsx
@@ -1,14 +1,21 @@
 "use client"
 
+import { Suspense } from "react"
+
 import { GamesNavigation } from "@/widgets/GamesNavigation"
 
 import { GamesLibrarySection } from "./components/gamesLibrarySection"
+//useSearchParams를 내부적으로 사용하고 있어 이를 위해서만 Suspense 사용
 
 export default function GamesPage() {
   return (
     <>
-      <GamesNavigation />
-      <GamesLibrarySection />
+      <Suspense fallback={null}>
+        <GamesNavigation />
+      </Suspense>
+      <Suspense fallback={null}>
+        <GamesLibrarySection />
+      </Suspense>
     </>
   )
 }
