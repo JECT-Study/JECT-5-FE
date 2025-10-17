92a394eda9d11bd0622c3f84beebf2a4713cc2f5
kimnamheeee <nami200284@snu.ac.kr>
2025-10-18 03:32:01 +0900

    chore: add unoptimized option to gif

--- service/app/src/app/create/loading.tsx
+++ service/app/src/app/create/loading.tsx
@@ -3,7 +3,13 @@ import Image from "next/image"
 export default function CreateGamePageSkeleton() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-16 bg-background-primary">
-      <Image src="/spinner.gif" alt="loading" width={100} height={100} />
+      <Image
+        src="/spinner.gif"
+        alt="loading"
+        width={100}
+        height={100}
+        unoptimized
+      />
       <p className="typography-heading-md-bold">
         페이지를 준비하고 있습니다...
       </p>
