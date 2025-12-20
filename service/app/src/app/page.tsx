17aa588613ec1b1eab6f6ba8c8959313c8cd88a4
kimnamheeee <nami200284@snu.ac.kr>
2025-12-20 18:01:01 +0900

    refactor(home): change home to server component

--- service/app/src/app/page.tsx
+++ service/app/src/app/page.tsx
@@ -1,30 +1,13 @@
-"use client"
-
-import { ErrorBoundary } from "react-error-boundary"
-
-import { useAuthStore } from "@/entities/auth"
 import { GameSection } from "@/widgets/GameSection"
 import { HeroSection } from "@/widgets/HeroSection"
 import { HomeNavigation } from "@/widgets/HomeNavigation"
 
-import ErrorPage from "./error"
-
-function HomeContent() {
-  const { isAuthenticated } = useAuthStore()
-
+export default function Home() {
   return (
     <main className="flex min-h-screen w-full flex-col items-start gap-112 bg-background-primary">
-      <HomeNavigation isLoggedIn={isAuthenticated} />
+      <HomeNavigation />
       <HeroSection />
       <GameSection />
     </main>
-  )
-}
-
-export default function Home() {
-  return (
-    <ErrorBoundary FallbackComponent={ErrorPage}>
-      <HomeContent />
-    </ErrorBoundary>
   )
 }
