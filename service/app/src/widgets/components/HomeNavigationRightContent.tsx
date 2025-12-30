bfb19899f3b4eec9b09451ffa67d618066977dd2
kimnamheeee <nami200284@snu.ac.kr>
2025-12-30 20:33:24 +0900

    fix(homenavigation): change component structure

--- service/app/src/widgets/components/HomeNavigationRightContent.tsx
+++ service/app/src/widgets/components/HomeNavigationRightContent.tsx
@@ -2,19 +2,16 @@
 
 import { PrimaryBoxButton } from "@ject-5-fe/design/components/button"
 import { Add } from "@ject-5-fe/design/icons"
-import dynamic from "next/dynamic"
+import Link from "next/link"
 
 import { useAuthStore } from "@/entities/auth"
 import { ThemeToggle } from "@/shared/themeToggleButton"
 
-const LoginButton = dynamic(() => import("@/shared/authButton"), {
-  ssr: false,
-})
-
-import Link from "next/link"
+import AvatarButton from "./avatarButton"
+import { KakaoLoginButton } from "./kakaoLoginButton"
 
 export default function HomeNavigationRightContent() {
-  const { authStatus } = useAuthStore()
+  const { authStatus, user, logout } = useAuthStore()
 
   return (
     <>
@@ -29,9 +26,10 @@ export default function HomeNavigationRightContent() {
               게임 만들기
             </Link>
           </PrimaryBoxButton>
+          <AvatarButton src={user?.profileImageUrl} onClick={logout} />
         </>
       ) : (
-        <LoginButton />
+        <KakaoLoginButton />
       )}
       <ThemeToggle />
     </>
