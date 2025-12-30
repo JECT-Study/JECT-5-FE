fce6808c1154c47959328ec900d8660101f74428
kimnamheeee <nami200284@snu.ac.kr>
2025-12-30 18:09:32 +0900

    chore(auth): remove unused logic

--- service/app/src/shared/api/fetchClient.ts
+++ service/app/src/shared/api/fetchClient.ts
@@ -31,7 +31,6 @@ export const instance = _instance.extend({
       },
       (_request, _options, response) => {
         if (response.status === 401 && typeof window !== "undefined") {
-          localStorage.removeItem("auth_user")
           document.cookie =
             "JSESSIONID=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/;"
           window.dispatchEvent(new CustomEvent("auth:session-expired"))
