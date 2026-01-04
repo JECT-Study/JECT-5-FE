a0630636188802787d1ed95e63c02b24bf92c059
kimnamheeee <nami200284@snu.ac.kr>
2026-01-04 19:29:37 +0900

    style(gamegrid): fix layout

--- service/app/src/app/games/components/gamesLibrarySection.tsx
+++ service/app/src/app/games/components/gamesLibrarySection.tsx
@@ -39,7 +39,7 @@ export function GamesLibrarySection() {
   const { openPreview } = useGamePreview()
 
   return (
-    <div className="mx-auto flex w-full max-w-[1130px] flex-col items-center px-10 sm:px-6 lg:px-0">
+    <div className="mx-auto flex w-full max-w-[1130px] flex-col items-center px-10 pb-120 pt-[210px] sm:px-6 lg:px-0">
       <div className="grid w-full grid-cols-1 gap-10 sm:grid-cols-3 lg:grid-cols-5 lg:gap-60">
         {isLoading && (!filteredGames || filteredGames.length === 0) && (
           <GameLibrarySkeleton count={19} />
