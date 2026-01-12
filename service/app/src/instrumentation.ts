285d6e96cfad72d4408e43cbaff91d1215e9c10a
kimnamheeee <nami200284@snu.ac.kr>
2026-01-12 13:52:54 +0900

    fix(instrumentation): enable msw only on development state

--- service/app/src/instrumentation.ts
+++ service/app/src/instrumentation.ts
@@ -1,5 +1,8 @@
 export async function register() {
-  if (process.env.NEXT_RUNTIME === "nodejs") {
+  if (
+    process.env.NEXT_RUNTIME === "nodejs" &&
+    process.env.NODE_ENV === "development"
+  ) {
     const { server } = await import("./mocks/server")
     server.listen()
   }
