2d84a273920bfabec396a017f03c5edecea7209c
kimnamheeee <nami200284@snu.ac.kr>
2026-01-12 15:04:13 +0900

    chore(ci): enable MSW for e2e tests

--- service/app/src/instrumentation.ts
+++ service/app/src/instrumentation.ts
@@ -1,7 +1,7 @@
 export async function register() {
   if (
     process.env.NEXT_RUNTIME === "nodejs" &&
-    process.env.NODE_ENV === "development"
+    process.env.MSW_ENABLED === "true"
   ) {
     const { server } = await import("./mocks/server")
     server.listen()
